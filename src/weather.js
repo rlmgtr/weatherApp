dfb9fc9d7de2022bbf13d1e11d168bc38be63b83
rlmgtr <rlmagtira@gmail.com>
2024-07-29 17:51:09 +0800

    Update weather.js

---
+++ src/weather.js
@@ -0,0 +1,14 @@
+function getWeatherInfo () {
+ const cityInput = document.getElementById("cityInput");
+ const cityName = document.getElementById("CityName");
+ 
+}
+
+fetch("https://api.openweathermap.org/data/2.5/weather?q='+cityName.value+'&appid=f1a7f601f87c9d97579ef8237cc83ff1")
+.then(res => res.json())
+.then(data => { 
+    const cityName = data.name;
+    document.getElementById("cityName").textContent = `${cityName}`
+
+
+})
