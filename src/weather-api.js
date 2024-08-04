b7bdcca8dc532ee4878e7346ed0f7f8b2d098fe0
rlmgtr <rlmagtira@gmail.com>
2024-08-04 17:38:39 +0800

    update

--- src/weather-api.js
+++ src/weather-api.js
@@ -1,21 +1,27 @@
-function weatherSource() {
-    const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
-    const API_KEY = "f1a7f601f87c9d97579ef8237cc83ff1";
-    const cityInput = document.getElementById('cityInput').value;
 
-    const weatherSourceURL = `${BASE_URL}?q=${cityInput}&appid=${API_KEY}`;
+function weatherSourceAll () {
 
-    fetch(weatherSourceURL)
-        .then(res => res.json())
-        .then(data => {
-            document.getElementById('cityName').textContent = `${data.name}`;
-            document.getElementById('temp').textContent = `${data.main.temp}`;
-            document.getElementById('min').textContent = `${data.main.temp_min}`;
-            document.getElementById('max').textContent = `${data.main.temp_max}`;
-        })
-        .catch(error => {
-            console.error('Error fetching the weather data:', error);
-        });
+    function weatherSource() {
+        const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
+        const API_KEY = "f1a7f601f87c9d97579ef8237cc83ff1";
+        const cityInput = document.getElementById('cityInput').value;
+    
+        const weatherSourceURL = `${BASE_URL}?q=${cityInput}&appid=${API_KEY}`;
+    
+        fetch(weatherSourceURL)
+            .then(res => res.json())
+            .then(data => {
+                document.getElementById('cityName').textContent = `${data.name}`;
+                document.getElementById('temp').textContent = `${data.main.temp}`;
+                document.getElementById('min').textContent = `${data.main.temp_min}`;
+                document.getElementById('max').textContent = `${data.main.temp_max}`;
+            })
+            .catch(error => {
+                console.error('Error fetching the weather data:', error);
+            });
+    
+        return false; // Prevent form submission
+    }
 
-    return false; // Prevent form submission
 }
+
